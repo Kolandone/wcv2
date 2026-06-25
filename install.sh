3ffa5673e16cf6282448c4076df12281fd300f55
KOLAND <sjamshid81@gmail.com>
2026-06-25 16:13:08 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ else
     EXT="tar.gz"
 fi
 
-BINARY="Worker-Creator-koland"
+BINARY="Worker-Creator-koland-${OS_LOWER}-${ARCH}.${EXT}"
 ARCHIVE="Worker-Creator-koland-${OS_LOWER}-${ARCH}.${EXT}"
 LATEST_VERSION=$(curl -fsSL https://raw.githubusercontent.com/Kolandone/wcv2/refs/heads/main/VERSION)
 
